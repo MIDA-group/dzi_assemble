784b3c71994f5e718024fd99c6d87a0e494a889e
Joakim Lindblad <joakim@cb.uu.se>
2025-02-05 23:28:49 +0100

    Code beautification; extracted dzi_info function

--- dzi_assemble.py
+++ dzi_assemble.py
@@ -11,8 +11,8 @@
 #         Width="7026"/>
 #</Image>
 
-import argparse
-import math
+import argparse, math, sys
+from collections import namedtuple
 import xml.etree.ElementTree as ET
 import pyvips
 
@@ -26,40 +26,51 @@ def get_args():
     parser.add_argument('--level','-l', metavar='LEVEL', type=int, default='0', help='Subsample level (steps up from 1:1); e.g., level=2 => 2 levels up = 4x4 subsampling')
     return parser.parse_args()
 
+# Convenience function to return named tuple with same name as function
+def returntuple(names, *args):
+    # or inspect.currentframe()
+    calling_fun = sys._getframe().f_back.f_code.co_name
+    return namedtuple(calling_fun, names)(*args)
 
-if __name__ == '__main__':
-    args = get_args()
-
-    tree = ET.parse(args.input)
+# Extract xml data from the dzi file
+def dzi_info(filename):
+    tree = ET.parse(filename)
     root = tree.getroot()
     format = root.get('Format')
     overlap = int(root.get('Overlap'))
     tilesize = int(root.get('TileSize'))
-    imgsize = {k: int(v) for k, v in root[0].attrib.items()} # Python-sigh!
-    print(overlap,tilesize,imgsize)
+    size = {k: int(v) for k, v in root[0].attrib.items()} # Python-sigh!
+    return returntuple("format,overlap,tilesize,size",format,overlap,tilesize,size)
+    
+if __name__ == '__main__':
+    args = get_args()
+    info = dzi_info(args.input)
+    print(info)
 
-    levels = max(imgsize['Height'],imgsize['Width']).bit_length()
+    levels = max(info.size['Height'],info.size['Width']).bit_length()
     level = levels - args.level
-    print(levels, level)
+    print(f'Assembling from level {level} = {levels}-{args.level}')
 
-    width=math.ceil(imgsize['Width']/(2**args.level))
-    height=math.ceil(imgsize['Height']/(2**args.level))
+    # Tiles wide, high
+    width=math.ceil(info.size['Width']/(2**args.level))
+    height=math.ceil(info.size['Height']/(2**args.level))
 
     # Filename: x_y.format
-    max_x=math.ceil(imgsize['Width']/(tilesize*2**args.level))
-    max_y=math.ceil(imgsize['Height']/(tilesize*2**args.level))
-    print(f'Tiling {max_x}x{max_y} images of size {tilesize} -> {width}x{height}')
+    max_x=math.ceil(info.size['Width']/(info.tilesize*2**args.level))
+    max_y=math.ceil(info.size['Height']/(info.tilesize*2**args.level))
+    print(f'Tiling {max_x}x{max_y} images of size {info.tilesize} -> {width}x{height}')
 
-    if overlap==0:
-        tiles = [pyvips.Image.new_from_file(f"{args.input.removesuffix('.dzi')}_files/{level}/{x}_{y}.{format}", access="sequential")
+    # Tile using pivyps
+    if info.overlap==0:
+        tiles = [pyvips.Image.new_from_file(f"{args.input.removesuffix('.dzi')}_files/{level}/{x}_{y}.{info.format}", access="sequential")
             for y in range(max_y) for x in range(max_x)]
     else: # Overlap imposes need for cropping
-        tiles = [pyvips.Image.new_from_file(f"{args.input.removesuffix('.dzi')}_files/{level}/{x}_{y}.{format}", access="sequential")
-                .crop(overlap if x>0 else 0, overlap if y>0 else 0, 
-                      tilesize if x+1<max_x else (width-1)%tilesize+1, tilesize if y+1<max_y else (height-1)%tilesize+1)
+        tiles = [pyvips.Image.new_from_file(f"{args.input.removesuffix('.dzi')}_files/{level}/{x}_{y}.{info.format}", access="sequential")
+                .crop(info.overlap if x>0 else 0, info.overlap if y>0 else 0, 
+                      info.tilesize if x+1<max_x else (width-1)%info.tilesize+1, info.tilesize if y+1<max_y else (height-1)%info.tilesize+1)
                 for y in range(max_y) for x in range(max_x)]
         
-    # Crop required to trim lower right image border
+    # Crop is required to trim lower right image border
     im = pyvips.Image.arrayjoin(tiles, across=max_x).crop(0,0,width,height)
 
     im.write_to_file(args.output)
